e378f58b8033aebbb33fb398d940661a0ec5ff87
norpulatovv <yoldoshevxasan98@gmail.com>
2025-06-20 16:06:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let ism = prompt("Ismingizni kiriting")
+
+let familiya = prompt("Familiyangizni kiriting")
+
+let yosh = prompt("Yoshingizni kiriting")
+
+let fan = prompt("Yoqtirgan faningizni yozing")
+
+let tuman = prompt("Yashaydigan tumanigizni kiriting")
+
+alert("malumotlaringizni kiritganingiz uchun tashakkur")
+
+console.log(ism)
+
+console.log(familiya)
+
+console.log(yosh)
+
+console.log(fan)
+
+console.log(tuman)
